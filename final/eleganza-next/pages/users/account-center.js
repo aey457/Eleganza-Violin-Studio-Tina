df51a8f12c1b8c8fcd3a3dc198e7223471ef9634
Loman <hanon242424@gmail.com>
2024-05-01 13:52:48 +0800

    account center大致好仍須微調

--- final/eleganza-next/pages/users/account-center.js
+++ final/eleganza-next/pages/users/account-center.js
@@ -8,53 +8,53 @@ export default function AccountCenter() {
 
    <>
       <Head>
-        <title>Eleganza Studio</title>
+        <title>Account Center</title>
         <meta name="viewport" content="width=device-width" />
       </Head>
-        <main>
+        <div>
          <div className={styles['mainarea']}>
          <div>
             <div>
-               <p className={styles.maintitle} >帳號細節</p>
-               <div className="formdetail">
-               <div className="formkey">
+               <p className={styles['maintitle']} >帳號細節</p>
+               <div className={styles['formdetail']} >
+               <div className={styles['formkey']} >
                   <p>帳號名稱</p>
                   <p>手機號碼</p>
                </div>
-               <div className="formvalue">
+               <div className={styles['formvalue']} >
                   <p>Fanny456@gmail.com</p>
                   <p>0987987452</p>
                </div>
                </div>
             </div>
             <div>
-               <p className="maintitle">個人檔案</p>
-               <div className="formdetail">
-               <div className="formkey">
+               <p className={styles['maintitle']}>個人檔案</p>
+               <div className={styles['formdetail']}>
+               <div className={styles['formkey']}>
                   <p>姓名</p>
                   <p>手機號碼</p>
                   <p>顯示名稱</p>
                </div>
-               <div className="formvalue">
-               <input className="formstyle" type="text" defaultValue="王貫中" />
-               <input className="formstyle" type="text" defaultValue="0987987452" /> {/* 將數字轉換為字串 */}
-               <input className="formstyle" type="text" defaultValue="Fanny456" />
+               <div className={styles['formvalue']}>
+               <input className={styles['formstyle']} type="text" defaultValue="王貫中" />
+               <input className={styles['formstyle']} type="text" defaultValue="0987987452" /> {/* 將數字轉換為字串 */}
+               <input className={styles['formstyle']} type="text" defaultValue="Fanny456" />
                </div>
                </div>
             </div>
             <div>
-               <p className="maintitle">變更密碼</p>
-               <div className="formdetail">
-               <div className="formkey">
+               <p className={styles['maintitle']}>變更密碼</p>
+               <div className={styles['formdetail']}>
+               <div className={styles['formkey']}>
                   <p>舊密碼</p>
                   <p>新密碼</p>
                   <p>密碼確認</p>
                </div>
-               <div className="formvalue">
-                  <input className="formstyle" type="password" defaultValue="" />
-                  <input className="formstyle" type="password" defaultValue="" />
-                  <input className="formstyle" type="password" defaultValue="" />
-                  <div className="xsbtn">
+               <div className={styles['formvalue']}>
+                  <input className={styles['formstyle']} type="password" defaultValue="" />
+                  <input className={styles['formstyle']} type="password" defaultValue="" />
+                  <input className={styles['formstyle']} type="password" defaultValue="" />
+                  <div className={styles['xsbtn']} >
                      <a href="">儲存</a>
                   </div>
                </div>
@@ -63,7 +63,7 @@ export default function AccountCenter() {
          </div>
          </div>
       
-      </main>
+      </div>
       
     </>
    
